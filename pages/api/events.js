748531f3195e9461b8295e284d60726bceeddc8e
Dennis Muensterer <dennismuensterer@gmail.com>
2020-07-26 05:18:35 +0200

    Add allowed origins

--- pages/api/events.js
+++ pages/api/events.js
@@ -2,6 +2,10 @@ import events from '../../events.json'
 
 export default (req, res) => {
   res.statusCode = 200
-  res.setHeader('Access-Control-Allow-Origin: *')
+  const origin = req.headers.origin
+  const allowedOrigins = ['http://localhost:3000', 'http://localhost:9000']
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
   res.json(events)
 }
